938f46bb674b6917e2e14932ff8869b34730cb2e
smjoys <iefh00@gmail.com>
2025-05-31 21:11:49 -0400

    pipeline searchp-service

--- backend/searchp-service/tests/integration/searchp.integration.test.js
+++ backend/searchp-service/tests/integration/searchp.integration.test.js
@@ -1,23 +1,19 @@
-const app = require("../../loginApp"); // o el path correcto
-const request = require("supertest");
+const app = require('../../searchApp');
+const request = require('supertest');
 
-describe("Pruebas de integración para el servicio de búsqueda", () => {
-    test("debe obtener un usuario", async () => {
-        const res = await request(app)
-            .post("/buscar")
-            .send({ comuna: "Chillan" });
 
-        expect(res.status).toBe(200);
-        expect(res.body).toEqual(expect.any(Array));
-        expect(res.body.length).toBeGreaterThan(0);
-    });
+test("debe obtener un usuario", async () => {
+    const res = await request(app).post("/buscar").send({ comuna: "Chillan" });
 
-    test("debe retornar 404 si no se encuentra el usuario", async () => {
-        const res = await request(app)
-            .post("/buscar")
-            .send({ comuna: "ComunaInventada" });
-
-        expect(res.status).toBe(404);
-        expect(res.body).toEqual({ mensaje: "Usuario no encontrado" });
-    });
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expect.any(Array));
+    expect(res.body.length).toBeGreaterThan(0);
 });
+
+test("debe retornar 404 si no se encuentra el usuario", async () => {
+    const res = await request(app).post("/buscar").send({ comuna: "ComunaInventada" });
+
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ mensaje: "Usuario no encontrado" });
+});
+
