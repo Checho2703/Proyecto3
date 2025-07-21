bd963027dcab92aa0c91f292a11c92b2ea0d8b3b
smjoys <iefh00@gmail.com>
2025-07-20 22:07:20 -0400

    cors implementation

--- backend/upload-service/upload_file.js
+++ backend/upload-service/upload_file.js
@@ -4,6 +4,7 @@ const fileUpload = require("express-fileupload");
 const express = require("express");
 const mysql = require("mysql2");
 const client = require("prom-client"); // Prometheus client
+const cors = require("cors"); 
 
 //########################## Express de archivo ##############################
 const extensionesValidas = ["pdf", "docx", "mp4"];
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(cors());
 
 //########################## MÉTRICAS PROMETHEUS ##############################
 const register = new client.Registry();
